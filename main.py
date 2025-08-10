912d51244eca31116a3ea419eb8e2d546aa1edd6
Daniil669 <daniil.freeman669@gmail.com>
2025-08-10 09:05:30 +0300

    class Player added, constants updated

--- main.py
+++ main.py
@@ -4,12 +4,20 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0)) #rgb 0, 0, 0 - black
+            
+        screen.fill("black")
         pygame.display.flip()
+
+        # limits to 60 fps
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
